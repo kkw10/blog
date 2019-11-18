850521114ce1ea5296761d221e0c0f7f9bd477ae
kkw_10 <kkw_10@naver.com>
2019-11-18 11:22:02 +0900

    [#41] 댓글 테이블 생성 및 연관 테이블과 연결
    - comment table 생성
    - User, Post 테이블과 연결
    
    Resolves #41

---
+++ backEnd/src/models/comment.js
@@ -0,0 +1,18 @@
+module.exports = (sequelize, DataTypes) => {
+  const Comment = sequelize.define('Comment', {
+    contents: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+  }, {
+    charset: 'utf8',
+    collate: 'utf8_general_ci'    
+  });
+
+  Comment.associate = (db) => {
+    db.Comment.belongsTo(db.User);
+    db.Comment.belongsTo(db.Post);
+  }
+
+  return Comment;
+}
--- backEnd/src/models/index.js
+++ backEnd/src/models/index.js
@@ -10,6 +10,7 @@ const sequelize = new Sequelize(config.database, config.username, config.passwor
 db.User = require('./user')(sequelize, Sequelize);
 db.Post = require('./post')(sequelize, Sequelize);
 db.HashTag = require('./hashTag')(sequelize, Sequelize);
+db.Comment = require('./comment')(sequelize, Sequelize);
 
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
